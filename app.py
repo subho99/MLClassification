a5929cbd9f203c1425b205266311ae81f4e0bb62
subho99 <subhajitbasistha@gmail.com>
2022-11-04 23:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 #import plotly.express as px
 import pickle
 
-import pandas_profiling
+#import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
